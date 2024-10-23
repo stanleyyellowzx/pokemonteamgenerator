1c5968fb7947c00f6eb4c59bd43a495c11f10f7f
forksticks <145156284+forksticks@users.noreply.github.com>
2024-10-23 17:12:39 -0400

    Add files via upload

---
+++ pokemonteamgenerator/js/test.js
@@ -0,0 +1,15 @@
+const numToString = {
+    1 : "one",
+    2 : "two",
+    3 : "three",
+    4 : "four",
+    5 : "five",
+    6 : "six"
+};
+
+console.log(numToString[1]);
+console.log(numToString[2]);
+console.log(numToString[3]);
+console.log(numToString[4]);
+console.log(numToString[5]);
+console.log(numToString[6]);
